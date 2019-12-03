96eb4db3e14f5fa07549bee3da1587a5eca5bcfd
Gökhan Demir <gdemir3327@gmail.com>
2019-12-03 14:49:34 +0300

    Update BkmController.php

--- backend/app/controllers/api/v4/BkmController.php
+++ backend/app/controllers/api/v4/BkmController.php
@@ -3,7 +3,7 @@
 class BkmController extends V4Controller {
 
   private static function _cache_search($query_url) {
-    $cache_name = "DR_SEARCH_" . $query_url;
+    $cache_name = "Bkm_search_" . $query_url;
 
     if (ApplicationCache::exists("$cache_name")) {
 
@@ -25,7 +25,7 @@ class BkmController extends V4Controller {
   }
 
   private static function _cache_bests($query_url) {
-    $cache_name = "DR_BESTS_" . $query_url;
+    $cache_name = "Bkm_bests_" . $query_url;
 
     if (ApplicationCache::exists("$cache_name")) {
 
@@ -44,6 +44,48 @@ class BkmController extends V4Controller {
     }
 
     return $data;
+  }
+
+  private static function _cache_detail($query_url) {
+    $cache_name = "Bkm_detail_" . $query_url;
+
+    if (ApplicationCache::exists("$cache_name")) {
+
+      $data = ApplicationCache::read("$cache_name");
+
+    } else {
+      // text search, fetch [data, data] or NULL
+
+      $data = self::_query_detail($query_url);
+
+      if ($data) {
+        ;// ApplicationCache::write("$cache_name", $data);
+      } else {
+        return NULL;
+      }
+    }
+
+    return $data;
+  }
+
+  public function detail() {
+
+    if (!isset($_POST["link"])) {
+      $json = self::_query_json_template(429, "Verilerde eksiklik var!");
+      return $this->render(["text" => $json], ["content_type" => "application/json"]);
+    }
+
+    $post_link = $_POST["link"];
+
+    $data = self::_cache_detail($post_link);
+
+    if ($data) {
+      $json = self::_query_json_template(200, "Başarılı istek", $data);
+      return $this->render(["text" => $json], ["content_type" => "application/json"]);
+    } else {
+      $json = self::_query_json_template(404, "Üzgünüm aradığım kaynaklarımda ürününüzü bulamadım.");
+      return $this->render(["text" => $json], ["content_type" => "application/json"]);
+    }
   }
 
   public function news() {
@@ -80,6 +122,17 @@ class BkmController extends V4Controller {
       $json = self::_query_json_template(404, "Üzgünüm aradığım kaynaklarımda ürününüzü bulamadım.");
       return $this->render(["text" => $json], ["content_type" => "application/json"]);
     }
+  }
+
+  private static function _query_detail($query_url) {
+    $file = file_get_contents($query_url);
+
+    preg_match_all("'<span itemprop=\"isbn\">(.*?)</span>'si", $file, $barcode);
+    $_barcode = $barcode[1];
+
+    return [
+      "barcode" => $_barcode
+    ];
   }
 
   private static function _query_search($query_url) {
